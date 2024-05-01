2e71a7771318a3050d2d53d19f042aa509bf89a7
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-01 15:05:45 -0400

    index route

--- server.js
+++ server.js
@@ -21,17 +21,40 @@ app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
-app.get('/entry/new', (req, res) => {
+app.get('/entries', async (req, res) => {
+    const allEntries = await Entry.find();
+
+    // Sorts all entries into arrays by each postedDay value
+    let entriesByDay = {}
+    allEntries.forEach( (entry) => {
+        // Creates array if there isn't one for that postedDay
+        if (entriesByDay[entry.postedDay] === undefined) {
+            entriesByDay[entry.postedDay] = []
+        }
+
+        // Adds entry to array with key value of postedDay
+        entriesByDay[entry.postedDay].push(entry)
+    })
+    
+    res.render('entry/index.ejs', {
+        entries: entriesByDay,
+        entriesKeys: Object.keys(entriesByDay)
+    })
+})
+
+app.get('/entries/new', (req, res) => {
     res.render('entry/new.ejs')
 })
 
-app.post('/entry', async (req, res) => {
+app.post('/entries', async (req, res) => {
     const newEntry = req.body
+    
+    // Change input strings to Nums
     newEntry.postedDay = parseInt(newEntry.postedDay)
     newEntry.amount = parseInt(newEntry.amount)
     
     await Entry.create(newEntry)
-    res.redirect('/')
+    res.redirect('/entries')
 })
 // ================ ROUTES ================== //
 
