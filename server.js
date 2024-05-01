646bc735de2af2bd5c721b9e5c4a7b745a09cc8b
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-01 11:44:33 -0400

    home page created

---
+++ server.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI)
+
+const methodOverride = require('method-override')
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static('public'))
+app.use(methodOverride('_method'))
+
+
+// ================ ROUTES ================== //
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+// ================ ROUTES ================== //
+
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database...')
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000...')
+})
